2d166750ac9d30e55a1c46c528de8414b3c461dd
demonchild2112 <denverlovesyou@gmail.com>
2017-08-26 19:44:59 +0200

    Add rc to path.

--- build_qx.ps1
+++ build_qx.ps1
@@ -2,7 +2,9 @@
 $binarycreator = 'C:\Qt\Tools\QtInstallerFramework\2.0\bin\binarycreator.exe'
 $env:Path += ';C:\Qt\5.9.1\msvc2017_64\bin'
 $env:Path += ';C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\amd64'
+$env:Path += ';C:\Program Files (x86)\Windows Kits\10\bin\x64'
 cd C:\projects\qaccelerator
 qmake
 nmake
 ls release
+ls 'C:\Program Files (x86)\Windows Kits'
