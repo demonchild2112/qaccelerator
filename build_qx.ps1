af03da23ace6385fa4d4a8549575bdb1dc146b44
demonchild2112 <denverlovesyou@gmail.com>
2017-08-26 15:07:03 +0200

    Add appveyor file.

--- build_qx.ps1
+++ build_qx.ps1
@@ -1,3 +1,6 @@
 ﻿$sevenzip = 'C:\Program Files\7-Zip\7z.exe'
 $binarycreator = 'C:\Qt\Tools\QtInstallerFramework\2.0\bin\binarycreator.exe'
-$qmake = 'C:\Qt\5.8\msvc2015_64\bin\qmake.exe'
+$qmake = 'C:\Qt\5.9.1\msvc2017_64\bin\qmake.exe'
+cd C:\qx
+$qmake
+git status
