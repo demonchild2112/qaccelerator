3126fe628b6ef50e65bffa762716dc1bc530ca6a
demonchild2112 <denverlovesyou@gmail.com>
2017-08-26 19:21:33 +0200

    Add qmake and nmake to path.

--- build_qx.ps1
+++ build_qx.ps1
@@ -1,8 +1,8 @@
 ﻿$sevenzip = 'C:\Program Files\7-Zip\7z.exe'
 $binarycreator = 'C:\Qt\Tools\QtInstallerFramework\2.0\bin\binarycreator.exe'
-$qmake = 'C:\Qt\5.9.1\msvc2017_64\bin\qmake.exe'
-$nmake = 'C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\amd64\nmake.exe'
+$env:Path += ';C:\Qt\5.9.1\msvc2017_64\bin'
+$env:Path += ';C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\amd64'
 cd C:\projects\qaccelerator
-$qmake
-$nmake
+qmake
+nmake
 ls release
