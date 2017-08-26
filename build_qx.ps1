4d7558b5ee9d3b92515c81767357e4101faaf80e
demonchild2112 <denverlovesyou@gmail.com>
2017-08-26 19:09:10 +0200

    Found nmake. See if it works.

--- build_qx.ps1
+++ build_qx.ps1
@@ -1,10 +1,8 @@
 ﻿$sevenzip = 'C:\Program Files\7-Zip\7z.exe'
 $binarycreator = 'C:\Qt\Tools\QtInstallerFramework\2.0\bin\binarycreator.exe'
 $qmake = 'C:\Qt\5.9.1\msvc2017_64\bin\qmake.exe'
+$nmake = 'C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\amd64\nmake.exe'
 cd C:\projects\qaccelerator
 $qmake
-ls 'C:\Program Files (x86)'
-ls 'C:\Program Files (x86)\Microsoft Visual Studio 14.0'
-ls 'C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\amd64'
-ls 'C:\Program Files\Microsoft Visual Studio 14.0\VC\bin\amd64'
+$nmake
 git status
