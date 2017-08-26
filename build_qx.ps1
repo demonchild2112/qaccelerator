3fb506f216e89639044d2757d07bc489886f88df
demonchild2112 <denverlovesyou@gmail.com>
2017-08-26 10:03:52 +0200

    nmake doesn't work.

--- build_qx.ps1
+++ build_qx.ps1
@@ -1,2 +1,3 @@
-﻿$sevenzip_binary = 'C:\Program Files\7-Zip\7z.exe'
-
+﻿$sevenzip = 'C:\Program Files\7-Zip\7z.exe'
+$binarycreator = 'C:\Qt\Tools\QtInstallerFramework\2.0\bin\binarycreator.exe'
+$qmake = 'C:\Qt\5.8\msvc2015_64\bin\qmake.exe'
