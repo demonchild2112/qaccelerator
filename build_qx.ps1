8c503fe5d090478171c1d743da7bcb5c8a2978b9
demonchild2112 <denverlovesyou@gmail.com>
2017-08-26 15:25:45 +0200

    Fix clone folder.

--- build_qx.ps1
+++ build_qx.ps1
@@ -1,6 +1,6 @@
 ﻿$sevenzip = 'C:\Program Files\7-Zip\7z.exe'
 $binarycreator = 'C:\Qt\Tools\QtInstallerFramework\2.0\bin\binarycreator.exe'
 $qmake = 'C:\Qt\5.9.1\msvc2017_64\bin\qmake.exe'
-cd C:\qx
+cd C:\projects\qaccelerator
 $qmake
 git status
