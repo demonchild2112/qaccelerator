d854cb66571fb09e00272fac3822cf2f05f1d767
demonchild2112 <denverlovesyou@gmail.com>
2017-08-26 19:05:39 +0200

    See if nmake exists in program files.

--- build_qx.ps1
+++ build_qx.ps1
@@ -3,5 +3,8 @@ $binarycreator = 'C:\Qt\Tools\QtInstallerFramework\2.0\bin\binarycreator.exe'
 $qmake = 'C:\Qt\5.9.1\msvc2017_64\bin\qmake.exe'
 cd C:\projects\qaccelerator
 $qmake
-nmake
+ls 'C:\Program Files (x86)'
+ls 'C:\Program Files (x86)\Microsoft Visual Studio 14.0'
+ls 'C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\amd64'
+ls 'C:\Program Files\Microsoft Visual Studio 14.0\VC\bin\amd64'
 git status
