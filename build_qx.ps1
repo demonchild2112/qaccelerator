ce5755ef3d18a5550b2b927fb4bcc118de962d45
demonchild2112 <denverlovesyou@gmail.com>
2017-08-27 12:28:00 +0200

    Get build env to work locally.

--- build_qx.ps1
+++ build_qx.ps1
@@ -1,7 +1,15 @@
 ﻿$sevenzip = 'C:\Program Files\7-Zip\7z.exe'
 $binarycreator = 'C:\Qt\Tools\QtInstallerFramework\2.0\bin\binarycreator.exe'
 cd C:\projects\qaccelerator
+
+# Prepare the build environment, and dump the vars to files so
+# we can load them here.
+& prepare_build_env.bat
+$env:PATH = cat env.path.txt
+$env:LIB = cat env.lib.txt
+$env:LIBPATH = cat env.libpath.txt
+$env:INCLUDE = cat env.include.txt
+
 qmake
 nmake
 ls release
-ls 'C:\Program Files (x86)\Windows Kits'
