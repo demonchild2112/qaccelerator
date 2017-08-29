152b3caf65d369db6a90548b80644234feefe99b
demonchild2112 <denverlovesyou@gmail.com>
2017-08-29 05:31:16 +0200

    About to start testing packaging.

--- build_qx.ps1
+++ build_qx.ps1
@@ -2,16 +2,13 @@
 $binarycreator = 'C:\Qt\Tools\QtInstallerFramework\2.0\bin\binarycreator.exe'
 cd C:\projects\qaccelerator
 
-# Prepare the build environment, and dump the vars to files so
-# we can load them here.
+# Prepare the build environment.
 & C:\projects\qaccelerator\prepare_build_env.bat
 $env:PATH = cat env.path.txt
 $env:LIB = cat env.lib.txt
 $env:LIBPATH = cat env.libpath.txt
 $env:INCLUDE = cat env.include.txt
-
 $env:PATH += ';C:\Qt\5.9.1\msvc2015_64\bin'
-# $env:PATH += ';C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\amd64'
 
 qmake
 nmake
